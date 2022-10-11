d4f9f20a08e59af7e9358a7c8fa7954a20572535
jpienaar-tuks <u24050505@tuks.co.za>
2022-10-11 10:12:09 +0200

    Did some stuff with line endings, because apparent Hadoop doesn't like CRLF

--- py/mapper.py
+++ py/mapper.py
@@ -5,7 +5,7 @@ from collections import namedtuple
 
 def mapper():
     for line in sys.stdin:
-        row=Line._make(line)
+        row=Line._make(line.split(', '))
         if row.STN=='STN':
             continue # Continue to next row if current row is a file heaader
         try:
@@ -24,28 +24,43 @@ def mapper():
         # some data count information, so that averages can be calculated afterwards
 
         #Let's start with precipitation:
-        print(f'DoubleValueSum:{country}.{row.YEAR}.{row.MONTH}.PRCP\t{row.PRCP}')
-        print(f'DoubleValueSum:{country}.{row.YEAR}.{row.MONTH}.PRCP_C\t1')
+        try:
+            float(row.PRCP)
+            print(f'DoubleValueSum:{country}.{row.YEAR}.{row.MONTH}.PRCP\t{row.PRCP.strip()}')
+            print(f'DoubleValueSum:{country}.{row.YEAR}.{row.MONTH}.PRCP_C\t1')
+        except ValueError:
+            pass
 
         #Let's move on to temperatures:
         if int(row.MONTH) in [1,2,6,7,8,12]:
-            season=season_dict[f'{hemisphere}-{row.MONTH}']
-            print(f'DoubleValueSum:{country}.{row.YEAR}.{season}.MAX_T\t{row.MAX_TEMP}')
-            print(f'DoubleValueSum:{country}.{row.YEAR}.{season}.MAX_T_C_C\t1')
-            print(f'DoubleValueSum:{country}.{row.YEAR}.{season}.MEAN_T\t{row.MEAN_TEMP}')
-            print(f'DoubleValueSum:{country}.{row.YEAR}.{season}.MEAN_T_C_C\t1')
-            print(f'DoubleValueSum:{country}.{row.YEAR}.{season}.MIN_T\t{row.MIN_TEMP}')
-            print(f'DoubleValueSum:{country}.{row.YEAR}.{season}.MIN_T_C_C\t1')
+            season=season_dict[f'{hemisphere}-{int(row.MONTH)}']
+            for variable, variable_text in zip([row.MAX_TEMP, row.MEAN_TEMP, row.MIN_TEMP],
+                                               ['MAX_T','MEAN_T','MIN_T']):
+                writeline(variable, variable_text, country, row.YEAR, season)
+            # print(f'DoubleValueSum:{country}.{row.YEAR}.{season}.MAX_T\t{row.MAX_TEMP}')
+            # print(f'DoubleValueSum:{country}.{row.YEAR}.{season}.MAX_T_C\t1')
+            # print(f'DoubleValueSum:{country}.{row.YEAR}.{season}.MEAN_T\t{row.MEAN_TEMP}')
+            # print(f'DoubleValueSum:{country}.{row.YEAR}.{season}.MEAN_T_C\t1')
+            # print(f'DoubleValueSum:{country}.{row.YEAR}.{season}.MIN_T\t{row.MIN_TEMP}')
+            # print(f'DoubleValueSum:{country}.{row.YEAR}.{season}.MIN_T_C\t1')
+
+def writeline(variable, variable_text, country, year, season):
+    try:
+        float(variable)
+        print(f'DoubleValueSum:{country}.{year}.{season}.{variable_text}\t{variable}')
+        print(f'DoubleValueSum:{country}.{year}.{season}.{variable_text}_C\t1')
+    except ValueError:
+        pass
+
 
 if __name__== "__main__":
-    with open('station_dict.pickle','rb') as f:
+    with open('station-dict.pickle','rb') as f:
         station_dict=pickle.load(f)
     Line = namedtuple('Line','STN, WBAN, YEAR, MONTH, DAY, MEAN_TEMP, MAX_TEMP, MIN_TEMP, PRCP')
-    mapper()
     season_dict={}
     for hemisphere in ['N','S']:
         for month in [1,2,6,7,8,12]:
-            if hemisphere='N':
+            if hemisphere=='N':
                 if month in [6,7,8]:
                     season_dict[f'{hemisphere}-{month}']='SUMMER'
                 else:
@@ -55,3 +70,4 @@ if __name__== "__main__":
                     season_dict[f'{hemisphere}-{month}']='WINTER'
                 else:
                     season_dict[f'{hemisphere}-{month}']='SUMMER'
+    mapper()
